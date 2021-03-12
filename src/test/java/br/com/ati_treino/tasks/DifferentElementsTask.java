fb72b5f5bd6784e5f277d86a7f071e995c4b8b4e
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-12 16:38:28 -0300

    Criei os metodos ações no elementos

--- src/test/java/br/com/ati_treino/tasks/DifferentElementsTask.java
+++ src/test/java/br/com/ati_treino/tasks/DifferentElementsTask.java
@@ -16,11 +16,37 @@ public class DifferentElementsTask {
     public DifferentElementsTask textArea() {
         differentElementsAppObjects.textAreaTextField().clear();
         differentElementsAppObjects.textAreaTextField().sendKeys("TESTE");
+//        differentElementsAppObjects.enviarTextAreaButton().click();
         return this;
     }
     public DifferentElementsTask textBox() {
         differentElementsAppObjects.firstNameTextBoxTextField().sendKeys("Jeferson");
         differentElementsAppObjects.lastNameTextBoxTextField().sendKeys("Lopes Eugenio");
+        return this;
+    }
+    public DifferentElementsTask radioCheckBox() {
+//        differentElementsAppObjects.noRadioCheckBox().click();
+        differentElementsAppObjects.otherRadioCheckBox().click();
+        return this;
+    }
+    public DifferentElementsTask checkbox3CheckBox() {
+        differentElementsAppObjects.checkbox3CheckBox().click();
+        return this;
+    }
+    public DifferentElementsTask inputTypeButton() {
+        differentElementsAppObjects.inputTypeButton().click();
+        driver.switchTo().alert().accept();
+        return this;
+    }
+    public DifferentElementsTask dropDownSelect() {
+        differentElementsAppObjects.listDropDownSelect().selectByValue("fiat");
+        differentElementsAppObjects.enviarDropDownButton().click();
+        driver.close();
+        return this;
+    }
+    public DifferentElementsTask webTable() {
+        System.out.println("Nome: " + differentElementsAppObjects.nameJohnWebTableTextLabel().getText());
+        System.out.println("Salario: " + differentElementsAppObjects.salary7000WebTableTextLabel().getText());
         return this;
     }
 }
