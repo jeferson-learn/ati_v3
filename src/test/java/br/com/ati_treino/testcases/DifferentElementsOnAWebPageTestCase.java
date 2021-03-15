205c9107b921debc4cfa232c7c113b64ddaadcc3
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-15 17:21:32 -0300

    Adicionei metodos para teste

--- src/test/java/br/com/ati_treino/testcases/DifferentElementsOnAWebPageTestCase.java
+++ src/test/java/br/com/ati_treino/testcases/DifferentElementsOnAWebPageTestCase.java
@@ -4,26 +4,34 @@ import br.com.ati_treino.frameworks.utlis.BaseTest;
 import br.com.ati_treino.tasks.DifferentElementsTask;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class DifferentElementsOnAWebPageTestCase extends BaseTest {
 
     @Test
-    public void test() throws InterruptedException {
+    public void test() throws InterruptedException, IOException {
         DifferentElementsTask differentElementsTask = new DifferentElementsTask(driver);
         driver.get("https://www.automationtestinginsider.com/2019/08/textarea-textarea-element-defines-multi.html");
-
         differentElementsTask
                 .textArea()
                 .textBox()
                 .radioCheckBox()
                 .checkbox3CheckBox()
                 .inputTypeButton()
-        //        .dropDownSelect()
+//                .dropDownSelect() /* Fecha aba */
                 .webTable()
                 .wikipediaFrames()
                 .datePicker()
                 .autoComplete()
                 .listBox()
                 .comboBox()
+                .doubleClickAndRightClick()
+//                .fileUpload() /* Tenta */
+                .tiposAlerts() /* parte prompt nao funciona preender */
+                .newBrowserWindow()
+                .testSeleniumWaits5Seconds()
+                .hiddenField()
+                .sameElement()
         ;
         Thread.sleep(5000);
     }
